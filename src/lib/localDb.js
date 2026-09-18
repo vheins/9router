32c21ee989a4b06c41165744d27dfd7e895de730
Muhammad Rheza Alfin <aRhez0903@gmail.com>
2026-09-18 16:46:15 +0700

    fix(db): export getQuotaSnapshots from localDb shim

--- src/lib/localDb.js
+++ src/lib/localDb.js
@@ -17,5 +17,6 @@ export {
   getCustomModels, addCustomModel, deleteCustomModel,
   getMitmAlias, setMitmAliasAll,
   getPricing, getPricingForModel, updatePricing, resetPricing, resetAllPricing,
+  getQuotaSnapshots,
   exportDb, importDb,
 } from "@/lib/db/index.js";
